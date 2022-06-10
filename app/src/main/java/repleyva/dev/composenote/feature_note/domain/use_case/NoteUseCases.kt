09066f18407633edb36143c07514f00468a4d1fb
repleyva <rusvelpassos@gmail.com>
2022-06-10 08:29:47 -0500

    add useCases in data class

--- app/src/main/java/repleyva/dev/composenote/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/repleyva/dev/composenote/feature_note/domain/use_case/NoteUseCases.kt
@@ -1,6 +1,6 @@
 package repleyva.dev.composenote.feature_note.domain.use_case
 
-data class NoteUseCase(
+data class NoteUseCases(
     val getNotes: GetNotesUseCase,
     val deleteNote: DeleteNoteUseCase
 )
